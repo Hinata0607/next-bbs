0e04b907df6e153df51f949634da4a97c960686a
Hinata0607 <atahin0607@gmail.com>
2024-05-27 06:02:51 +0900

    created BBS get api

---
+++ app/api/post/[bbsId]/route.tsx
@@ -0,0 +1,12 @@
+import prisma from "@/lib/prismaClient";
+import { NextResponse } from "next/server";
+
+// URLに含まれるパラメータのためstring型
+export async function GET(req: Request, { params }: { params: { bbsId: string } }) {
+    const bbsId = params.bbsId
+    // findUniqueメソッドで一件取得(条件はwhere句)
+    const bbsDetailData = await prisma.post.findUnique({where: {
+        id: parseInt(bbsId),
+    }});
+    return NextResponse.json(bbsDetailData);
+}
